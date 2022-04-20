0756403407c82e9df5ed40bcf2e875b44cdf0ae7
Sarrg <j.kieslich@aon.at>
2022-04-20 11:38:10 +0200

    WIP Commit of Sort Mod

--- Themes/Rebirth/Scripts/00 ThemeInfo.lua
+++ Themes/Rebirth/Scripts/00 ThemeInfo.lua
@@ -1,8 +1,8 @@
 THEMEINFO = {
-    Name = "Rebirth",
+    Name = "Rebirth (Sort Mod)",
     Version = "2",
-    Date = "20220313",
-    Author = "Celebelian, poco0317, and the Etterna Team"
+    Date = "20220412",
+    Author = "Celebelian, poco0317, and the Etterna Team (Modded by Sarrg)"
 }
 
 function getThemeName()
